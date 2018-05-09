223e81ff026b622fd5fbdfa5b55d770b0b7c2d54
kevin12686 <kevin860612@gmail.com>
2018-05-10 00:33:10 +0800

    memory cost down

--- parallel_analysis.py
+++ parallel_analysis.py
@@ -15,7 +15,6 @@ if __name__ == '__main__':
     # N_list = [int(n) for n in input('Number of Elements (Separate with comma): ')]
     # Python 3
     # N_list = [n for n in input('Number of Elements (Separate with blank): ').replace(' ', '').split(',')]
-    random_list = []
     process_list = []
     N_list = [50000, 100000, 150000, 200000, 250000, 300000]
     N_list.sort(reverse=True)
@@ -23,12 +22,12 @@ if __name__ == '__main__':
         test_data = list(range(each_N))
         for idx in range(times):
             random.shuffle(test_data)
-            random_list.append(copy.deepcopy(test_data))
-            bub = Process(target=critical_section, args=(bubble_sort, random_list[-1]))
-            sel = Process(target=critical_section, args=(selection_sort, random_list[-1]))
-            ins = Process(target=critical_section, args=(insertion_sort, random_list[-1]))
-            qui = Process(target=critical_section, args=(quick_sort, random_list[-1]))
-            hea = Process(target=critical_section, args=(heap_sort, random_list[-1]))
+            temp = copy.copy(test_data)
+            bub = Process(target=critical_section, args=(bubble_sort, temp))
+            sel = Process(target=critical_section, args=(selection_sort, temp))
+            ins = Process(target=critical_section, args=(insertion_sort, temp))
+            qui = Process(target=critical_section, args=(quick_sort, temp))
+            hea = Process(target=critical_section, args=(heap_sort, temp))
             process_list.append(bub)
             process_list.append(sel)
             process_list.append(ins)
